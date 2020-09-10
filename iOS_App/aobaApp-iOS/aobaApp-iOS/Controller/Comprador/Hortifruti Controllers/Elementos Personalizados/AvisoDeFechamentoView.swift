884a02811a48a8f3fb171bb4a98522f8ee598792
CarlosModinez <modinez.carlos@gmail.com>
2020-09-10 09:41:00 -0300

    Aviso de fechamento adicionado

---
+++ iOS_App/aobaApp-iOS/aobaApp-iOS/Controller/Comprador/Hortifruti Controllers/Elementos Personalizados/AvisoDeFechamentoView.swift
@@ -0,0 +1,21 @@
+//
+//  AvisoDeFechamentoView.swift
+//  aobaApp-iOS
+//
+//  Created by Carlos Modinez on 10/09/20.
+//  Copyright © 2020 M Cavasin. All rights reserved.
+//
+
+import UIKit
+
+class AvisoDeFechamentoView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
