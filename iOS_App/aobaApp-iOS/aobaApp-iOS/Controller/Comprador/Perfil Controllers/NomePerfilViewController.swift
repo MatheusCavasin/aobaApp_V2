f968da5acbd429fcf4dd117f1c0db51002c95992
CarlosModinez <modinez.carlos@gmail.com>
2020-10-27 07:29:48 -0300

    Perfil e perfil request adicionados, mas ainda com server error nao tratado

--- iOS_App/aobaApp-iOS/aobaApp-iOS/Controller/Comprador/Perfil Controllers/NomePerfilViewController.swift
+++ iOS_App/aobaApp-iOS/aobaApp-iOS/Controller/Comprador/Perfil Controllers/NomePerfilViewController.swift
@@ -11,20 +11,27 @@ import UIKit
 class NomePerfilViewController: UIViewController {
 
     @IBOutlet weak var txtNome: UITextField!
+    @IBOutlet weak var txtNomeFantasia: UITextField!
     @IBOutlet weak var btnCancelar: UIButton!
     @IBOutlet weak var btnAlterar: UIButton!
     @IBOutlet weak var viewLoadView: UIView!
     
+    private let repository = CompradorRepository()
     
     override func viewDidLoad() {
         super.viewDidLoad()
         txtNome.text = Singleton.shared.comercianteLogado?.nome
+        txtNomeFantasia.text = Singleton.shared.comercianteLogado?.nomeFantasia
         viewLoadView.isHidden = true
     }
 
     @IBAction func btnCancelarPressed(_ sender: Any) {
         self.dismiss(animated: true, completion: nil)
     }
+    
     @IBAction func btnAlterarPressed(_ sender: Any) {
+        let perfil = PerfilData(emailAntigo: nil, emailNovo: nil, nome: txtNome.text, nomeFantasia: txtNomeFantasia.text, senhaAntiga: nil, novaSenha: nil)
+        
+        repository.editarPerfil(perfil: perfil)
     }
 }
