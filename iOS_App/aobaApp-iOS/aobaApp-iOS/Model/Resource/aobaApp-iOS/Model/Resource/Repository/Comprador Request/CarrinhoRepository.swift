630da8a442f4d2bf5c55f63008794c89e8409851
Matheus Cavasin <matheus.scavasin@gmail.com>
2020-11-14 13:02:20 -0300

    correcao bugs

--- iOS_App/aobaApp-iOS/aobaApp-iOS/Model/Resource/aobaApp-iOS/Model/Resource/Repository/Comprador Request/CarrinhoRepository.swift
+++ iOS_App/aobaApp-iOS/aobaApp-iOS/Model/Resource/aobaApp-iOS/Model/Resource/Repository/Comprador Request/CarrinhoRepository.swift
@@ -21,6 +21,16 @@ class CarrinhoRepository {
             completion(result, err)
         }
     }
+    
+    func fazerPedido(carrinhoPedido: CarrinhoPedido,
+                     completion: @escaping (Any?, Error?) -> Void) {
+        let url = "https://aoba-api-server.herokuapp.com/api/v1/pedido"
+        let carrinhoPedidoDict = carrinhoPedido.objectToDict()
+        ApiResource.request(method: "POST", url: url, params: nil, body: carrinhoPedidoDict, withAuth: true) { (result, err) in
+            completion(result, err)
+        }
+    }
+    
     func getCarrinhoVendedor(completion: @escaping (Any?, Error?) -> Void) {
         
         print("request carrinho")
@@ -33,36 +43,3 @@ class CarrinhoRepository {
     }
 }
 
-func getAnuncios(){
-    print("requestAnuncios")
-    
-    //Coloque a URL da sua API aqui
-    let url = "https://aoba-api-server.herokuapp.com/api/v1/anuncio/produtor/3"
-    
-    ApiResource.request(method: "GET", url: url, params: nil, body: nil, withAuth: true){
-        (result, err)  in
-        //Aqui você tem seu resultado
-        if let res:Bool = (err == nil) {
-            if(res) {
-                //Aqui res podera assumir dois valores, true ou false
-                print("sua requisicao foi realizada com sucesso")
-                ModelVendedor.instance.dictListaAnuncios = result as! [[String : Any?]]
-                DispatchQueue.main.async{
-                    NotificationCenter.default.post(name: Notification.Name(rawValue: "NotificationID"), object: nil)
-                }
-                
-            } else {
-                //Aqui voce pode tratar os erros
-                print("a requisicao nao funcionou")
-                print(err)
-            }
-    
-    func fazerPedido(carrinhoPedido: CarrinhoPedido,
-                     completion: @escaping (Any?, Error?) -> Void) {
-        let url = "https://aoba-api-server.herokuapp.com/api/v1/pedido"
-        let carrinhoPedidoDict = carrinhoPedido.objectToDict()
-        ApiResource.request(method: "POST", url: url, params: nil, body: carrinhoPedidoDict, withAuth: true) { (result, err) in
-            completion(result, err)
-        }
-    }
-}
