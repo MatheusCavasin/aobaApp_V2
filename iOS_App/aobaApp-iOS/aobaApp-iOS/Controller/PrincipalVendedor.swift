687209c8d364391159e63a94332521dc95ed18a9
Matheus Cavasin <matheus.scavasin@gmail.com>
2020-11-27 07:08:10 -0300

    Remoção da tab bar perfil

--- iOS_App/aobaApp-iOS/aobaApp-iOS/Controller/PrincipalVendedor.swift
+++ iOS_App/aobaApp-iOS/aobaApp-iOS/Controller/PrincipalVendedor.swift
@@ -25,22 +25,23 @@ class PrincipalVendedor: UITabBarController {
 
         let storyboard = UIStoryboard(name: "TabAnuncioVendedor", bundle: nil)
         let storyboard1 = UIStoryboard(name: "TabPedidosVendedor", bundle: nil)
-        let storyboard2 = UIStoryboard(name: "TabPerfilVendedor", bundle: nil)
+//        let storyboard2 = UIStoryboard(name: "TabPerfilVendedor", bundle: nil)
 
         let firstBookTableVc = storyboard.instantiateViewController(withIdentifier: "TabAnuncioVendedor")
         let secondBookTableVc = storyboard1.instantiateViewController(withIdentifier: "TabPedidosVendedor")
-        let thirdBookTableVc = storyboard2.instantiateViewController(withIdentifier: "TabPerfilVendedor")
+//        let thirdBookTableVc = storyboard2.instantiateViewController(withIdentifier: "TabPerfilVendedor")
 
         //configure the view controllers here...
 
-        viewControllers = [firstBookTableVc, secondBookTableVc, thirdBookTableVc]
+//        viewControllers = [firstBookTableVc, secondBookTableVc, thirdBookTableVc]
+        viewControllers = [firstBookTableVc, secondBookTableVc]
 
 //        tabBar.items?[0].image = [first tab bar image]
 //        tabBar.items?[1].image = [second tab bar image]
 
         tabBar.items?[0].title = "Anuncios"
         tabBar.items?[1].title = "Pedidos"
-        tabBar.items?[2].title = "Perfil"
+//        tabBar.items?[2].title = "Perfil"
     }
     
     
